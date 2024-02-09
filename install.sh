55eb0bbc8aebddf44cf86faf997610a139ddc3c9
Dormant Chicken <150866822+dormant-chicken@users.noreply.github.com>
2024-02-09 17:18:50 -0500

    Fix directory paths in install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,11 @@ echo "Creating /usr/local/bin/sortty-bin/"
 
 sudo mkdir /usr/local/bin/sortty-bin/
 
-echo "Moving main.py into /usr/local/bin/"
+echo "Moving main.py from src into /usr/local/bin/"
 
-sudo cp main.py /usr/local/bin/sortty-bin/
+sudo cp src/main.py /usr/local/bin/sortty-bin/
 
-echo "moving sortty.sh into /usr/local/bin/"
+echo "moving sortty.sh from src into /usr/local/bin/"
 
 sudo touch /usr/local/bin/sortty
-sudo cp sortty.sh /usr/local/bin/sortty
-
-echo
+sudo cp src/sortty.sh /usr/local/bin/sortty
