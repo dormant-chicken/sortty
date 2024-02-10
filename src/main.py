e96fe7abb1490c310e20c818daa47f7f96e51d43
Dormant Chicken <150866822+dormant-chicken@users.noreply.github.com>
2024-02-10 17:15:09 -0500

    🚨 Yet another update to main.py 🚨
    Changelog:
    
    - Added insertion sort algorithm
    - Program gives error if terminal is too small for specified array size / array range

--- src/main.py
+++ src/main.py
@@ -90,17 +90,20 @@ def merge_sort(stdscr, array_size, wait_delay, term_height, startx, arr, is_main
             if is_main == True:
                 draw_array(sorted, stdscr, array_size, wait_delay, term_height, startx, arr)
 
+def insertion_sort(stdscr, array_size, wait_delay, term_height, startx, arr):
+    for i in range(1, len(arr)):
+        j = i
+        while arr[j] < arr[j - 1] and j > 0:
+            arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            j -= 1
+            draw_array(sorted, stdscr, array_size, wait_delay, term_height, startx, arr)
+
+
 def main(stdscr):
     # Finds terminal info
     term_size = os.get_terminal_size()
     term_height = term_size.lines
     term_width = term_size.columns
-
-    # Curses initialization
-    curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    stdscr.clear()
 
     # Set variables and lists
     array_size = int(sys.argv[1])
@@ -130,38 +133,67 @@ def main(stdscr):
 
     # Finds correct position to start drawing
     startx = math.floor(term_width / 2) - math.floor(array_size)
-    
-    # Draw the array before sorting
-    if wait_delay:
-        draw_array(sorted, stdscr, array_size, wait_delay, term_height, startx, array)
 
-    start_time = time.perf_counter()
+    # Curses initialization
+    curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.clear()
 
-    # Sorting algorithms determined by bash script
-    if sys.argv[5] == "bogosort":
-        bogo_sort(stdscr, array_size, wait_delay, term_height, startx, array)
-    elif sys.argv[5] == "bubblesort":
-        bubble_sort(stdscr, array_size, wait_delay, term_height, startx, array)
-    elif sys.argv[5] == "mergesort":
-        merge_sort(stdscr, array_size, wait_delay, term_height, startx, array, True)
+    # Quits program if terminal height too small for array_range
+    if (array_range + 2) > term_height:
+        stdscr.addstr(0, 0, "terminal height too small for array range!")
+        stdscr.addstr(2, 0, "press any key to exit")
+        stdscr.getch()
+        curses.endwin()
 
-    end_time = time.perf_counter()
+    # Same if terminal width too small for array_size
+    elif (array_size + 2) > term_width:
+        stdscr.addstr(0, 0, "terminal width too small for array size!")
+        stdscr.addstr(2, 0, "press any key to exit")
+        stdscr.getch()
+        stdscr.endwin
 
-    # Only draw at the end of sorting when wait_delay is on, otherwise it would just show the array being unsorted
-    if not wait_delay:
-        draw_array(sorted, stdscr, array_size, wait_delay, term_height, startx, array)
-    
-    # Shows that array is sorted and other info
-    stdscr.addstr(0, 0, "Array sorted!")
-    stdscr.addstr(2, 0, "Sorting information:")
-    stdscr.addstr(4, 0, "sorting algorithm: " + str(sys.argv[5]))
-    stdscr.addstr(5, 0, "array size: " + str(array_size))
-    stdscr.addstr(6, 0, "array range: " + str(array_range))
-    stdscr.addstr(7, 0, "time taken to sort: " + str(math.floor(end_time - start_time)) + " second(s)")
-    stdscr.addstr(9, 0, "Press any key to exit")
-    
-    # Waits for key press and stops program
-    stdscr.getch()
-    curses.endwin()
+    # Otherwise, start main script
+    else:
+        # Draw the array before sorting
+        if wait_delay:
+            draw_array(sorted, stdscr, array_size, wait_delay, term_height, startx, array)
+
+        # Starts performance timer
+        start_time = time.perf_counter()
+
+        # Sorting algorithms determined by bash script
+        if sys.argv[5] == "bogosort":
+            bogo_sort(stdscr, array_size, wait_delay, term_height, startx, array)
+
+        elif sys.argv[5] == "bubblesort":
+            bubble_sort(stdscr, array_size, wait_delay, term_height, startx, array)
+
+        elif sys.argv[5] == "mergesort":
+            merge_sort(stdscr, array_size, wait_delay, term_height, startx, array, True)
+
+        elif sys.argv[5] == "insertionsort":
+            insertion_sort(stdscr, array_size, wait_delay, term_height, startx, array)
+        
+        # Ends performance timer
+        end_time = time.perf_counter()
+
+        # Only draw at the end of sorting when wait_delay is on, otherwise it would just show the array being unsorted
+        if not wait_delay:
+            draw_array(sorted, stdscr, array_size, wait_delay, term_height, startx, array)
+        
+        # Shows that array is sorted and other info
+        stdscr.addstr(0, 0, "Array sorted!")
+        stdscr.addstr(2, 0, "Sorting information:")
+        stdscr.addstr(4, 0, "sorting algorithm: " + str(sys.argv[5]))
+        stdscr.addstr(5, 0, "array size: " + str(array_size))
+        stdscr.addstr(6, 0, "array range: " + str(array_range))
+        stdscr.addstr(7, 0, "time taken to sort: " + str(math.floor(end_time - start_time)) + " second(s)")
+        stdscr.addstr(9, 0, "Press any key to exit")
+        
+        # Waits for key press and stops program
+        stdscr.getch()
+        curses.endwin()
 
 wrapper(main)
