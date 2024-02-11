d38be83668e5637d01ef8b83a04fd7febb0a487f
Dormant Chicken <150866822+dormant-chicken@users.noreply.github.com>
2024-02-11 09:40:36 -0500

    Sync main.py updates with sortty.sh

--- src/sortty.sh
+++ src/sortty.sh
@@ -2,19 +2,9 @@
 
 version=v1.0-stable
 
-help_message () {
-  echo
-  echo "usage: sortty [ -h --help ] (to show advanced options) [ -v --version ] (to show version)"
-  echo
-  echo "or"
-  echo
-  echo "usage: sortty [ array_size ] [ array_range] [ fill ] [ wait_time (milliseconds) ] [ algorithm ] (to use the script)"
-  echo
-  echo "example command: sortty 15 10 0 100 bubblesort"
-  echo
-}
-
 help_advanced () {
+  # Displays sorTTY in big ascii letters using figlet
+  figlet sorTTY
   echo
   echo "usage: sortty [ -h --help ] (to show advanced options)"
   echo
@@ -24,34 +14,36 @@ help_advanced () {
   echo
   echo "or"
   echo
-  echo "sortty [ array_size ] [ array_range] [ fill ] [ wait_time (milliseconds) ] [ algorithm ] (to use the script)"
-  echo
-  echo "Enter the value types as follows:"
-  echo "sortty [ integer ] [ integer ] [ boolean integer (0 or 1) ] [ integer ] [ string ]"
+  echo "sortty [ array_size (integer) ] [ array_range (integer) ] [ fill (boolean integer (0 or 1)) ] [ reversed (boolean integer) ] [ show_info (boolean integer) ] [ wait_time (milliseconds) (integer) ] [ algorithm (string) ]"
   echo
   echo "Example command:"
-  echo "sortty 15 10 0 100 bubblesort"
+  echo "sortty 15 10 0 0 1 100 bubblesort"
   echo
   echo "In the example command above,"
   echo
   echo "[ array_size ] is 15, meaning it will give the program 15 items to sort."
   echo
-  echo "[ array_range ] is 10, meaning the array that the program sorts ranges from values 0 to 10."
+  echo "[ array_range ] is 10, meaning the array that the program sorts ranges from values 1 to 10."
   echo
-  echo "[ fill ] is 0 (AKA False), meaning that the program will not ignore the values above and use the highest possible array size and array range, limited by the size of the screen."
+  echo "[ fill ] is 0 (AKA False), but if it is 1 (AKA True), the program will ignore the values above and use the highest possible"
+  echo "array size and array range, limited by the size of the screen, basically meaning it fills the screen with the array"
+  echo
+  echo "[ reversed ] is 0 (AKA False), so the program will not sort from greatest to least, but least to greatest."
+  echo
+  echo "[ show_info ] is 1 (AKA True), so the program will show the sorting information after sorting"
   echo
   echo "[ algorithm ] uses the bubblesort algorithm, but available algorithms are: bogosort, bubblesort, mergesort, insertionsort, quicksort"
   echo
-  echo "Note: If [ fill ] is 1 (AKA True), the program will ignore [ array_size ] and [ array_range ], as it makes the program use the screen dimensions for [ array_size ] and [ array_range ] instead."
+  echo "Note: If [ fill ] is 1 (AKA True), the program will ignore [ array_size ] and [ array_range ],"
+  echo "as it makes the program use the screen dimensions for [ array_size ] and [ array_range ] instead."
   echo
-  echo 
 }
 
 # If first argument is empty, show help message
 if [ -z "$1" ]; then
   echo
   echo "Arguments are missing"
-  help_message
+  help_advanced
 
 # -h or --help arguments
 elif [ $1 == "-h" ] || [ $1 == "--help" ]; then
@@ -64,13 +56,13 @@ elif [ $1 == "-v" ] || [ $1 == "--version" ]; then
   echo "$version"
 
 # Checks if arguments are correct value type [ integer, integer, integer, integer, string ]
-elif [[ $1 =~ ^[0-9]+$ ]] && [[ $2 =~ ^[0-9]+$ ]] && [[ $3 =~ ^[0-9]+$ ]] && [[ $4 =~ ^[0-9]+$ ]] && ([ $5 == "bogosort" ] || [ $5 == "bubblesort" ] || [ $5 == "mergesort" ] || [ $5 == "insertionsort" ] || [ $5 == "quicksort" ]); then
-  python3 /usr/local/bin/sortty-bin/main.py $1 $2 $3 $4 $5
+elif [[ $1 =~ ^[0-9]+$ ]] && [[ $2 =~ ^[0-9]+$ ]] && ([ $3 == 0 ] || [ $3 == 1 ]) && ([ $4 == 0 ] || [ $4 == 1 ]) && ([ $5 == 0 ] || [ $5 == 1 ]) && [[ $6 =~ ^[0-9]+$ ]] && ([ $7 == "bogosort" ] || [ $7 == "bubblesort" ] || [ $7 == "mergesort" ] || [ $7 == "insertionsort" ] || [ $7 == "quicksort" ]); then
+  python3 /usr/local/bin/sortty-bin/main.py $1 $2 $3 $4 $5 $6 $7
 
 # Any other possible arguments display help message
 else
   echo
   echo "Argument(s) not recognized"
-  help_message
+  help_advanced
 
 fi
