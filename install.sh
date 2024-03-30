879f7f58cc254e53e730809204f2362cd205f085
Dormant Chicken <150866822+dormant-chicken@users.noreply.github.com>
2024-03-30 14:51:40 -0400

    Update install.sh to v1.7

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
-echo "Creating /usr/local/bin/sortty-bin/"
+echo "Creating /usr/local/bin/sortty-bin/ ..."
+echo "You might have to enter your root password"
 
 sudo mkdir /usr/local/bin/sortty-bin/
 
-echo "Moving main.py from src into /usr/local/bin/sortty-bin/"
+echo "Moving main.py from src into /usr/local/bin/sortty-bin/ ..."
 
 sudo cp src/main.py /usr/local/bin/sortty-bin/
 
-echo "Moving sortty.sh from src into /usr/local/bin/"
+echo "Moving sortty.py from src into /usr/local/bin/ ..."
+
+sudo touch /usr/local/bin/sortty-bin/sortty.py
+sudo cp src/sortty.sh /usr/local/bin/sortty-bin/sortty.py
+
+echo "Moving sortty.sh from src into /usr/local/bin/ ..."
 
 sudo touch /usr/local/bin/sortty
 sudo cp src/sortty.sh /usr/local/bin/sortty
 
-echo "Making /usr/local/bin/sortty executable"
+echo "Making /usr/local/bin/sortty executable ..."
 sudo chmod +x /usr/local/bin/sortty
 
 echo
