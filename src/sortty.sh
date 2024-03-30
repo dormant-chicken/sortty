2f50d9a3ade26975f141c827225e9fe7609425e9
Dormant Chicken <150866822+dormant-chicken@users.noreply.github.com>
2024-03-30 19:17:38 -0400

    Update sortty.sh

--- src/sortty.sh
+++ src/sortty.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 src/sortty.py $1 $2 $3 $4 $5 $6
+if [ -d /usr/local/bin/sortty-bin/ ]; then
+  python3 /usr/local/bin/sortty-bin/sortty.py $1 $2 $3 $4 $5 $6
+else
+  python3 src/sortty.py $1 $2 $3 $4 $5 $6
+fi
