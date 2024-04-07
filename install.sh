4a8e4db08dd0636e203db54d841c3e40bc13b7d4
qsser <140076645+qsser@users.noreply.github.com>
2024-04-07 20:37:23 +0200

    Update install.sh
    -installs to local user directory
    -asks if you want to add it to path
    -no alias

--- install.sh
+++ install.sh
@@ -1,40 +1,47 @@
 #!/bin/bash
+set -e
 
-if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "/usr/bin/bash" ] || [ "$SHELL" == "/bin/zsh" ] || [ "$SHELL" == "/usr/bin/zsh" ]; then
-    echo "Creating /usr/local/bin/sortty-bin/"
-    echo
+function yes_or_no { # ask yes or no function
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) return  1 ;;
+        esac
+    done
+}
 
-    echo "You might have to enter your root password"
-    echo
-    sudo mkdir /usr/local/bin/sortty-bin/
+echo "Creating ~/.local/bin/"
+mkdir -p ~/.local/bin
 
-    echo "Moving main.py from src into /usr/local/bin/sortty-bin/"
-    echo
-    sudo cp src/main.py /usr/local/bin/sortty-bin/
+# echo "You might have to enter your root password"
+#not anymore, all installed for the current user
+# echo
 
-    echo "Moving sortty.py from src into /usr/local/bin/sortty-bin/"
-    echo
-    sudo touch /usr/local/bin/sortty-bin/sortty.py
-    sudo cp src/sortty.py /usr/local/bin/sortty-bin/sortty.py
+echo "Copying sortty.py from src into ~/.local/bin"
+cp src/sortty.py ~/.local/bin/sortty #note it has no extention (so that you can call it by typing 'sortty')
+chmod +x ~/.local/bin/sortty
 
-    # places alias in rc file depending on shell
+# echo
+# echo
+
+#ask if you want to add directory to PATH
+if yes_or_no "Add ~/.local/bin to PATH?"; then
     if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "/usr/bin/bash" ]; then
-        echo "Appending 'alias sortty='python3 /usr/local/bin/sortty.py'' to ~/.bashrc so that sortty can be run directly from the terminal"
-        echo "alias sortty='python3 /usr/local/bin/sortty-bin/sortty.py'" >> ~/.bashrc
-        
-    elif [ "$SHELL" == "/bin/zsh" ] || [ "$SHELL" == "/usr/bin/zsh" ]; then
-        echo "Appending 'alias sortty='python3 /usr/local/bin/sortty.py'' to ~/.zshrc so that sortty can be run directly from the terminal"
-        echo "alias sortty='python3 /usr/local/bin/sortty-bin/sortty.py'" >> ~/.zshrc
-        
-    fi
+        echo "Appending 'export PATH=\$PATH:\$HOME/.local/bin' to ~/.bashrc so that sortty can be run directly from the terminal"
+        echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc
+        echo "Done, reopen your terminal so that you can run sortty directly from your terminal"
 
-    echo
-    echo "Done, reopen your terminal so that you can run sortty directly from your terminal"
-    echo
-    echo "Run 'sortty --help' for instructions"
-    echo
-    echo "Have fun using sortty!"
-    echo
-else
-    echo "Unrecognized shell (only bash and zsh are supported)"
+    elif [ "$SHELL" == "/bin/zsh" ] || [ "$SHELL" == "/usr/bin/zsh" ]; then
+        echo "Appending 'export PATH=\$PATH:\$HOME/.local/bin' to ~/.zshrc so that sortty can be run directly from the terminal"
+        echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.zshrc
+        echo "Done, reopen your terminal so that you can run sortty directly from your terminal"
+
+    else
+        echo "Unrecognized shell (only bash and zsh are supported)"
+    fi
 fi
+echo "Done!"
+echo "Run 'sortty --help' for instructions"
+echo
+echo "Have fun using sortty!"
