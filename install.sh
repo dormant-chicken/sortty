336152d6ef51e536cbc35775827aa37add9fe6d2
Dormant Chicken <150866822+dormant-chicken@users.noreply.github.com>
2024-05-24 17:20:58 -0400

    Clean up install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -e
 
-function yes_or_no { # ask yes or no function
+# ask yes or no function
+function yes_or_no {
     while true; do
         read -p "$* [y/n]: " yn
         case $yn in
-            [Yy]*) return 0  ;;  
-            [Nn]*) return  1 ;;
+            [Yy]*) return 0
+            ;;
+            [Nn]*) return 1
+            ;;
         esac
     done
 }
@@ -14,33 +17,32 @@ function yes_or_no { # ask yes or no function
 echo "Creating ~/.local/bin/"
 mkdir -p ~/.local/bin
 
-# echo "You might have to enter your root password"
-#not anymore, all installed for the current user
-# echo
-
 echo "Copying sortty.py from src into ~/.local/bin"
-cp src/sortty.py ~/.local/bin/sortty #note it has no extention (so that you can call it by typing 'sortty')
+# note it has no extention (so that you can call it by typing 'sortty')
+cp src/sortty.py ~/.local/bin/sortty
 chmod +x ~/.local/bin/sortty
+echo
 
-# echo
-# echo
-
-#ask if you want to add directory to PATH
+# ask if you want to add directory to PATH
 if yes_or_no "Add ~/.local/bin to PATH?"; then
     if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "/usr/bin/bash" ]; then
+        echo
         echo "Appending 'export PATH=\$PATH:\$HOME/.local/bin' to ~/.bashrc so that sortty can be run directly from the terminal"
         echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.bashrc
-        echo "Done, reopen your terminal so that you can run sortty directly from your terminal"
+        echo "reopen your terminal so that you can run sortty directly from your terminal"
 
     elif [ "$SHELL" == "/bin/zsh" ] || [ "$SHELL" == "/usr/bin/zsh" ]; then
+        echo
         echo "Appending 'export PATH=\$PATH:\$HOME/.local/bin' to ~/.zshrc so that sortty can be run directly from the terminal"
         echo "export PATH=\$PATH:\$HOME/.local/bin" >> ~/.zshrc
-        echo "Done, reopen your terminal so that you can run sortty directly from your terminal"
+        echo "reopen your terminal so that you can run sortty directly from your terminal"
 
     else
         echo "Unrecognized shell (only bash and zsh are supported)"
     fi
 fi
+
+echo
 echo "Done!"
 echo "Run 'sortty --help' for instructions"
 echo
